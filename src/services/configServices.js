6bcf56e9fe0474bf7802a4bd755526976b972186
sosamatias1 <sosamatias171@gmail.com>
2023-06-28 10:53:10 -0300

    Se modifica el configServices

--- src/services/configServices.js
+++ src/services/configServices.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://6464e4e3127ad0b8f8ab352a.mockapi.io/api/v1',
+    baseURL: 'http://localhost:8080/api/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
+        
     }
 })
 
 export default {
     async deleteProduct(id) {
         try{
-            await apiClient.delete('/E-commerceRopa/' + id)
+            await apiClient.delete('/products/' + id)
         }catch(e){
             throw "Error at trying to delete a product"
         }
     },
     async loadProducts(){
         try{
-            const response = await apiClient.get('/E-commerceRopa');
+            const response = await apiClient.get('/products');
             return response.data
         } catch(e) {
             throw "Error with the load";
@@ -26,21 +27,58 @@ export default {
     },
     async addProduct(elem){
         try{
-            await apiClient.post('/E-commerceRopa', elem)
+            await apiClient.post('/products', elem)
         }catch(e) {
             throw "Error at adding a product"
         }
     },
     async updateProduct(id, elem){
         try{
-            await apiClient.put('/E-commerceRopa/' + id, elem)
+            await apiClient.put('/products/' + id, elem)
         }catch(e){
             throw "Error at updating a product"
         }
     },
     async getProductById(id){
         try{
-            const product = await apiClient.get('/E-commerceRopa/' + id);
+            const product = await apiClient.get('/products/' + id);
+            return product.data
+        } catch(e) {
+            throw "Error with the getting the product";
+        }
+    },
+    async deleteUser(id) {
+        try{
+            await apiClient.delete('/users/' + id)
+        }catch(e){
+            throw "Error at trying to delete a product"
+        }
+    },
+    async loadUsers(){
+        try{
+            const response = await apiClient.get('/users');
+            return response.data
+        } catch(e) {
+            throw "Error with the load";
+        }
+    },
+    async addUser(elem){
+        try{
+            await apiClient.post('/users', elem)
+        }catch(e) {
+            throw "Error at adding a product"
+        }
+    },
+    async updateUser(id, elem){
+        try{
+            await apiClient.put('/users/' + id, elem)
+        }catch(e){
+            throw "Error at updating a product"
+        }
+    },
+    async getUserById(id){
+        try{
+            const product = await apiClient.get('/users/' + id);
             return product.data
         } catch(e) {
             throw "Error with the getting the product";
